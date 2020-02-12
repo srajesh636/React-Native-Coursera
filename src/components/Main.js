f5c48b8529775359bcf31d3e699dbf6ba3be78b4
U Sai Rajesh <srajesh636@gmail.com>
2020-02-13 00:52:56 +0530

    chore(app): adds dish detail component :sparkle:

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import Menu from "./Menu";
 import { DISHES } from "../data/dishes";
+import Dishdetail from "./DishDetail";
+import { View } from "react-native";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      selectedDish: null
     };
   }
 
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+
   render() {
-    return <Menu dishes={this.state.dishes} />;
+    return (
+      <View>
+        <Menu
+          dishes={this.state.dishes}
+          onPress={dishId => this.onDishSelect(dishId)}
+        />
+        <Dishdetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+      </View>
+    );
   }
 }
 
